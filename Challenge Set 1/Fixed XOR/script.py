5be5f5896588adde7c2f5c79a4f779fa359d8d9e
R4GN4R <jimchr2001@gmail.com>
2020-02-25 15:14:35 -0500

    Update script.py

--- Challenge Set 1/Fixed XOR/script.py
+++ Challenge Set 1/Fixed XOR/script.py
@@ -5,9 +5,9 @@ b = '686974207468652062756c6c277320657965'
 def XOR(a_hex,b_hex):
   a_binary = bin(int(a_hex, 16))[2:] # convert from hex to binary
   b_binary = bin(int(b_hex, 16))[2:] # convert from hex to binary
-  a_binaryXOR_b_binary = int(a_binary,2) ^ int(b_binary,2) # a_binaryXOR_b_binary is in decimal format
-  a_binaryXOR_b_binary = hex(a_binaryXOR_b_binary).split('x')[-1] # convert from decimal to hex
-  print(a_binaryXOR_b_binary)
+  a_binary_XOR_b_binary = int(a_binary,2) ^ int(b_binary,2) # a_binaryXOR_b_binary is in decimal format
+  a_binary_XOR_b_binary = hex(a_binary_XOR_b_binary).split('x')[-1] # convert from decimal to hex
+  print(a_binary_XOR_b_binary)
 
 
 XOR(a,b)
